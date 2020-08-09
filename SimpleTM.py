46fcefaa1d965b83fdab44b07ad5bbe5b5d14a83
jszhtian <jszhtian@hotmail.com>
2020-08-09 23:53:54 +0200

    Init Upload

---
+++ SimpleTM.py
@@ -0,0 +1,93 @@
+import sqlite3
+from sqlite3 import Error
+import os
+import sys
+
+
+class SimpleTM:
+    def __init__(self,sSqliteFileName):
+        self.__sSqliteFileName=sSqliteFileName
+        isExist=os.path.isfile(sSqliteFileName)
+        if(isExist==False):
+            self.__InitSQLite(self.__sSqliteFileName)
+        try:
+            self.__conn=sqlite3.connect(self.__sSqliteFileName)
+        except Error as e:
+            raise RuntimeError(e)
+    def __InitSQLite(self,sSqliteFileName):
+        try:
+            self.__conn=sqlite3.connect(self.__sSqliteFileName)
+            c=self.__conn.cursor()
+            c.execute('''
+            CREATE TABLE SimpleTM
+            (
+                GameTitle text,
+                RawWord text,
+                TranslatedWord text,
+                unique (GameTitle,RawWord) 
+            )
+            ''')
+            self.__conn.commit()
+            self.__conn.close()
+        except Error as e:
+            raise RuntimeError(e)
+    def Query(self,sRawWord):
+        try:
+            c=self.__conn.cursor()
+            c.execute('''
+            select RawWord,TranslatedWord,GameTitle
+            from SimpleTM
+            where instr(?,RawWord) > 0
+            ''',(sRawWord.lower(),))
+            self.__conn.commit()
+            rows=c.fetchall()
+            return rows
+        except Error as e:
+            raise RuntimeError(e)
+
+    def Insert(self,sRawWord,sTranslatedWord,sGameTitle):
+        try:
+            c=self.__conn.cursor()
+            c.execute('''
+            insert into SimpleTM(GameTitle,RawWord,TranslatedWord)
+            values (?,?,?)
+            ''', (sGameTitle.lower(),sRawWord.lower(),sTranslatedWord.lower()))
+            if c.rowcount < 1:
+                self.__conn.rollback()
+                return False
+            else:
+                self.__conn.commit()
+                return True
+        except Error as e:
+            raise RuntimeError(e)
+    def Update(self,sRawWord,sTranslatedWord,sGameTitle):
+        try:
+            c=self.__conn.cursor()
+            c.execute('''
+            update SimpleTM
+            set TranslatedWord=?
+            where GameTitle=? and RawWord=?
+            ''', (sTranslatedWord.lower(),sGameTitle.lower(),sRawWord.lower()))
+            if c.rowcount < 1:
+                self.__conn.rollback()
+                return False
+            else:
+                self.__conn.commit()
+                return True
+        except Error as e:
+            raise RuntimeError(e)
+    def Close(self):
+        self.__conn.close()
+if __name__ == "__main__":
+#SimpleTest
+    objSimpleTM=SimpleTM('SimpleTM.db')
+    print(objSimpleTM.Insert('Test','测试','TestGame1'))
+    print(objSimpleTM.Insert('Argument','理由','TestGame1'))
+    print(objSimpleTM.Insert('Test','测验','TestGame2'))
+    try:
+        print(objSimpleTM.Insert('Test','测试','TestGame1'))
+    except Exception as e:
+        print(e)
+    print(objSimpleTM.Update('Test','测试2','TestGame1'))
+    print(objSimpleTM.Update('Test','测试3','TestGame1'))
+    print(objSimpleTM.Query('reason'))
---
+++ wgsi.py
@@ -0,0 +1,68 @@
+from SimpleTM import SimpleTM
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Just a hello world!</h1>"
+@app.route('/api/query', methods=['GET'])
+def api_query():
+    if 'raw' in request.args:
+        rawWord=str(request.args['raw'])
+        
+        try:
+            SimpleTMObj=SimpleTM('SimpleTM.db')
+            ret=SimpleTMObj.Query(rawWord)
+            SimpleTMObj.Close()
+            json_lst=[]
+            for line in ret:
+                tmp_json_dict={}
+                tmp_json_dict['raw:']=line[0]
+                tmp_json_dict['translate:']=line[1]
+                tmp_json_dict['Game:']=line[2]
+                json_lst.append(tmp_json_dict)
+            return jsonify(json_lst)
+        except Exception as e:
+            return jsonify(Result='False',Message=str(e))
+    else:
+        return jsonify(Result='False',Message="No Valid Args find")
+@app.route('/api/insert', methods=['GET'])
+def api_insert():
+    if 'raw' in request.args and 'translate' in request.args and 'game' in request.args:
+        rawWord=str(request.args['raw'])
+        translate=str(request.args['translate'])
+        game=str(request.args['game'])
+        try:
+            SimpleTMObj=SimpleTM('SimpleTM.db')
+            ret=SimpleTMObj.Insert(rawWord,translate,game)
+            SimpleTMObj.Close()
+            if ret == True:
+                return jsonify(Result='True',Message='')
+            else:
+                return jsonify(Result='False',Message='')
+        except Exception as e:
+            return jsonify(Result='False',Message=str(e))
+    else:
+        return jsonify(Result='False',Message="No Valid Args find")
+@app.route('/api/update', methods=['GET'])
+def api_update():
+    if 'raw' in request.args and 'translate' in request.args and 'game' in request.args:
+        rawWord=str(request.args['raw'])
+        translate=str(request.args['translate'])
+        game=str(request.args['game'])
+        try:
+            SimpleTMObj=SimpleTM('SimpleTM.db')
+            ret=SimpleTMObj.Update(rawWord,translate,game)
+            SimpleTMObj.Close()
+            if ret == True:
+                return jsonify(Result='True',Message='')
+            else:
+                return jsonify(Result='False',Message='')
+        except Exception as e:
+            return jsonify(Result='False',Message=str(e))
+    else:
+        return jsonify(Result='False',Message="No Valid Args find")
+app.run()
