1a4ab9877678a9ad70dca8dfd59ed39e2a3d9484
Sicheng Jiang <ja235711@gmail.com>
2023-11-07 22:12:25 -0800

    online content editor

--- static/project.js
+++ static/project.js
@@ -1,37 +1,21 @@
 
 console.log('project.js loaded')
-function hello() {
-    return 'world'
-}
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', async function() {
     console.log('dom loaded')
-    hello();
-    const newKVButton = document.getElementById('new-kv-submit');
-    newKVButton.addEventListener('click', function() {
-        console.log('hello')
-        const infoElem = document.getElementById('form-info');
-        infoElem.innerHTML = ''
-        const k = document.getElementById('input-key').value;
-        const v = document.getElementById('input-value').value;
-        if (!k || !v) {
-            infoElem.innerHTML = '原文译文不能为空'
-        }
-        console.log(k, v)
+    const base_url = document.getElementById('secret-base-url-block').innerHTML;
+    const user = document.getElementById('secret-user-block').innerHTML;
+    const game = document.getElementById('secret-game-block').innerHTML;
+    const token = document.getElementById('secret-token-block').innerHTML;
+    const infoElem = document.getElementById('info-div');
 
-        const base_url = document.getElementById('secret-base-url-block').innerHTML;
-        const user = document.getElementById('secret-user-block').innerHTML;
-        const game = document.getElementById('secret-game-block').innerHTML;
-        const token = document.getElementById('secret-token-block').innerHTML;
-
-
-        let url = `${base_url}/api/insert/${game}/${k}/${v}`;
-        url = encodeURI(url);
-
+    function makeRequest(url) {
         const headers = new Headers({
             'Authorization': 'Basic ' + btoa(user + ':' + token)
         });
 
-        fetch(url, {
+        infoElem.innerHTML = '';
+
+        return fetch(url, {
             method: 'GET',
             headers: headers
         })
@@ -39,13 +23,94 @@ document.addEventListener('DOMContentLoaded', function() {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-            return true;
+            return response.json();
         })
         .catch(error => {
             infoElem.innerHTML = `${error}`
         });
-        
-        
-        infoElem.innerHTML = '';
+    }
+
+    async function addRow(raw, translate) {
+        const row = document.createElement('tr');
+        const ktd = document.createElement('td');
+        const kelem = document.createElement('input');
+        ktd.appendChild(kelem);
+        kelem.disabled = true;
+        kelem.value = raw;
+        row.k_value = raw;
+
+        const vtd = document.createElement('td');
+        const velem = document.createElement('input');
+        vtd.appendChild(velem);
+        velem.value = translate;
+        velem.k_value = kelem.value;
+        row.appendChild(ktd);
+        row.appendChild(vtd);
+        tb.appendChild(row);
+
+        const delBtn = document.createElement('button');
+        delBtn.k_value = kelem.value;
+        delBtn.innerText = '删除';
+        delBtn.addEventListener("click", async function() {
+            const k = this.k_value;
+            const r = await makeRequest(encodeURI(`${base_url}/api/delete/${game}/${k}`));
+            if (r && r.Result == "True") {
+                const tb = document.getElementById('content-table-body');
+                for (let i = 0; i < tb.children.length; i++) {
+                    const row = tb.children[i];
+                    if (row.k_value == k) {
+                        row.remove();
+                    }
+                }
+                for(let i = 0; i < dict.length; i++) {
+                    if (k == dict[i].raw) {
+                        dict.splice(i, 1);
+                        break;
+                    }
+                }
+            }
+        })
+        row.appendChild(delBtn);
+
+        velem.addEventListener("blur", async function() {
+            const k = this.k_value;
+            const v = this.value;
+            if (!v) {
+                infoElem.innerHTML = '译文不能为空';
+                return;
+            }
+            await makeRequest(encodeURI(`${base_url}/api/update/${game}/${k}/${v}`));
+        });
+    }
+    
+    const tb = document.getElementById('content-table-body');
+    const dict = await makeRequest(encodeURI(`${base_url}/api/querybygame/${game}`));
+    console.log(dict)
+
+    for(const {raw, translate} of dict) {
+        addRow(raw, translate);
+    }
+
+    const newKVButton = document.getElementById('new-kv-submit');
+    newKVButton.addEventListener('click', async function() {
+        infoElem.innerHTML = ''
+        const k = document.getElementById('input-key').value;
+        const v = document.getElementById('input-value').value;
+        if (!k || !v) {
+            infoElem.innerHTML = '原文译文不能为空'
+        }
+        console.log(k, v)
+        for(const {raw, translate} of dict) {
+            if (k == raw) {
+                infoElem.innerHTML = '原文不能重复';
+                return;
+            }
+        }
+        const r = await makeRequest(encodeURI(`${base_url}/api/insert/${game}/${k}/${v}`));
+        if (r && r.Result == "True") {
+            addRow(k, v);
+            dict.push({"raw": k, "translate": v});
+        }
     })
+
 });
